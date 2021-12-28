7844780299871d92d21fa64c56575c49d1cf864b
developerMahian <developermahian@gmail.com>
2021-12-28 13:14:25 +0600

    Navigation updated and unnessecary link actions prevented

--- public/app.js
+++ public/app.js
@@ -3,18 +3,25 @@
 // Hamburger Menu......
 const hamMenuBtn = document.getElementById("nav-toggler");
 const hamNav = document.getElementById("mobile-nav");
+const allAnchor = document.querySelectorAll("footer a");
 
 hamMenuBtn.addEventListener("click", () => {
-    hamNav.classList.toggle("opacity-100");
-    hamNav.classList.toggle("scale-100");
+	hamNav.classList.toggle("opacity-100");
+	hamNav.classList.toggle("scale-100");
 });
 
 // FAQ Accordion........
 const accordionBtn = document.querySelectorAll(".question-btn");
 
 accordionBtn.forEach((el) => {
-    el.addEventListener("click", () => {
-        el.classList.toggle("arrow-rotate");
-        el.nextElementSibling.classList.toggle("show-answer");
-    });
+	el.addEventListener("click", () => {
+		el.classList.toggle("arrow-rotate");
+		el.nextElementSibling.classList.toggle("show-answer");
+	});
+});
+
+// Stopping anchor links.....
+allAnchor.forEach((element) => {
+	console.log(element);
+	element.addEventListener("click", (e) => e.preventDefault());
 });
